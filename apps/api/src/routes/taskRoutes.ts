be10d047bdf83264db28f82b4491c7da345604cb
Shelby Deerman <srdeerman@crimson.ua.edu>
2026-07-30 20:59:48 -0500

    separated task and project concerns

---
+++ apps/api/src/routes/taskRoutes.ts
@@ -0,0 +1,99 @@
+import { Router, Request, Response, NextFunction } from 'express';
+import { TaskService } from "../services/taskService";
+import { BadRequestError } from '../errors';
+import { authenticateToken } from '../middleware/authentication';
+
+const router = Router();
+
+router.get('/', authenticateToken, async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const result = await TaskService.getAllTasks();
+        res.status(200).json(result);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.get('/:id', authenticateToken, async (req: Request, res: Response, next: NextFunction) => {
+    const requestedID = Number(req.params.id);
+    try {
+        const result = await TaskService.getTaskByID(requestedID);
+        res.status(200).json(result);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.post('/', authenticateToken, async (req: Request, res: Response, next: NextFunction) => {
+    const title = req.body?.title?.trim();
+    const description = req.body?.description?.trim();
+    const status = req.body?.status?.trim();
+    const projectID = req.body?.project_id?.trim();
+    const assignedTo = req.body?.assigned_to?.trim();
+    try {
+        const result = await TaskService.createTask(title, description, status, projectID, assignedTo);
+        res.status(201).json(result);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.patch('/:id', authenticateToken, async (req: Request, res: Response, next: NextFunction) => {
+    const requestedID = Number(req.params.id);
+    try {
+        if ("title" in req.body) {
+            const title = req.body?.title?.trim();
+            if (!title) {
+                throw new BadRequestError("A title is required.");
+            }
+            const result = await TaskService.updateTaskTitle(title, requestedID);
+            res.status(200).json(result);
+        }
+        else if ("description" in req.body) {
+            const description = req.body?.description?.trim();
+            if (!description) {
+                throw new BadRequestError("A description is required.");
+            }
+            const result = await TaskService.updateTaskDescription(description, requestedID);
+            res.status(200).json(result);
+        }
+        else if ("status" in req.body) {
+            const status = req.body?.status?.trim();
+            if (!status) {
+                throw new BadRequestError("A status is required.");
+            }
+            const result = await TaskService.updateTaskStatus(status, requestedID);
+            res.status(200).json(result);
+        }
+        else if ("project_id" in req.body) {
+            const projectID = req.body?.project_id?.trim();
+            if (!projectID) {
+                throw new BadRequestError("A project ID is required.");
+            }
+            const result = await TaskService.updateTaskProjectID(projectID, requestedID);
+            res.status(200).json(result);
+        }
+        else if ("assigned_to" in req.body) {
+            const assignedTo = req.body?.assigned_to?.trim();
+            if (!assignedTo) {
+                throw new BadRequestError("A user ID assigned to this task is required.");
+            }
+            const result = await TaskService.updateTaskUserID(assignedTo, requestedID);
+            res.status(200).json(result);
+        }
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.delete('/:id', authenticateToken, async (req: Request, res: Response, next: NextFunction) => {
+    const requestedID = Number(req.params.id);
+    try {
+        const result = await TaskService.deleteTask(requestedID);
+        res.status(204).json({ status: "Successfully deleted" });
+    } catch (error) {
+        next(error);
+    }
+});
+
+export default router;
